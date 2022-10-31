69bb5a66834b2d325b30984939b622892a1017d8
micah_z <micah.a.zahn@gmail.com>
2022-10-31 07:34:55 -0500

    setup basic file structure

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const {authMiddleware} = require('./utils/auth');
+const path = require('path');
+// Database connection
+const db = require('./config/connection');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
